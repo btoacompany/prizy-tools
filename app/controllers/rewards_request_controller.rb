660b0ef3a2d1dcb4655cf00a970732fb2001b1d8
sayorik <s.karakama@btoa-company.com>
2016-11-06 15:38:32 +0900

    rewards tool

--- app/controllers/rewards_request_controller.rb
+++ app/controllers/rewards_request_controller.rb
@@ -1,5 +1,6 @@
-class RewardsController < ApplicationController
+class RewardsRequestController < ApplicationController
   before_filter :authenticate_user, :init_url
+
   def index 
     @request_rewards = RequestReward.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,21 @@ Rails.application.routes.draw do
   post	'/admin/login/complete'   => "top#login_complete"
   get	'/admin/logout'		  => "top#logout"
 
-  get	'/admin/company'	  => 'company#index'
-  get	'/admin/company/:id'	  => 'company#posts'
-  get	'/admin/company/edit/:id' => 'company#edit'
-  post	'/admin/company/edit/complete' => 'company#edit_complete'
-  post	'/admin/company/delete'	  => 'company#delete'
+  get	'/admin/company'		=> 'company#index'
+  get	'/admin/company/:id'		=> 'company#posts'
+  get	'/admin/company/edit/:id'	=> 'company#edit'
+  post	'/admin/company/edit/complete'	=> 'company#edit_complete'
+  post	'/admin/company/delete'		=> 'company#delete'
 
-  get	'/admin/rewards'	  => 'rewards#index'
-  post	'/admin/rewards'	  => 'rewards#index'
+  get	'/admin/rewards_request'	  => 'rewards_request#index'
+  post	'/admin/rewards_request'	  => 'rewards_request#index'
+
+  get	'/admin/rewards_prizy'			=> 'rewards_prizy#index'
+  get	'/admin/rewards_prizy/add'		=> 'rewards_prizy#add'
+  post	'/admin/rewards_prizy/add/complete'	=> 'rewards_prizy#add_complete'
+  get	'/admin/rewards_prizy/edit/:id'		=> 'rewards_prizy#edit'
+  post	'/admin/rewards_prizy/edit/complete'	=> 'rewards_prizy#edit_complete'
+  post	'/admin/rewards_prizy/delete'		=> 'rewards_prizy#delete'
 
   # analytics or statistics
   get	'/admin/analytics/users'      => 'analytics#users'
